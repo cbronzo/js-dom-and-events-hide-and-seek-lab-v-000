bbc73aa040fc850036bf79d9cdf54f89ea78af0f
Catherine Bronzo <cbronzo@mac.com>
2019-03-23 02:33:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 
 function increaseRankBy(n){
   const rankedList = document.querySelectorAll('.ranked-list')
-  for (let i = 0; i < rankedList.length i++) {
+  for (let i = 0; i < rankedList.length; i++) {
     rankedList[i].innerHTML = (i + 1).toString()
   }
 } 
