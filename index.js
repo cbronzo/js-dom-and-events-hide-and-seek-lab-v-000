990209dd0e3c1e944251b59843601f646098a76d
Catherine Bronzo <cbronzo@mac.com>
2019-03-23 02:31:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+
+
+
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node
+}
